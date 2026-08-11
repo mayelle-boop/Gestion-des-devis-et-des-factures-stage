2deb54bcb294fce6233f90e1506d6332c004b55e
mayelle-boop <aliciank059@gmail.com>
2026-08-11 12:51:02 -0400

    feat: config.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from DevisFact.models import db
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
+    db.init_app(DevisFact)
     @app.route('/')
     def accueil():
         return "L'application fonctionne ! 🚀"
-    return app
+    return DevisFact
 
 
 
